89d111e7a72754736dfb34458351753cdf61ee67
KarenVT <karenkvot@gmail.com>
2023-06-21 17:22:15 -0500

    nueva clase UtilidadPopUps para darle funcionalidad a los popups

---
+++ src/main/java/com/g02/juegofx1/UtilidadPopUps.java
@@ -0,0 +1,14 @@
+package com.g02.juegofx1;
+
+import java.io.IOException;
+
+public class UtilidadPopUps {
+    
+    public static void showCorrectPopup() {
+
+    }
+
+    public static void showIncorrectPopup() {
+
+    }
+}
