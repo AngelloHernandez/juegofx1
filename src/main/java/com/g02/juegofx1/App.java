b18528a39b25c8bc47cf30ad4b00588949639c51
KarenVT <karenkvot@gmail.com>
2023-06-22 14:49:43 -0500

    Aplicar efecto blur en las vetanas emergentes de popup

--- src/main/java/com/g02/juegofx1/App.java
+++ src/main/java/com/g02/juegofx1/App.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+    static Scene scene;
     public static User user;
 
     @Override
--- src/main/java/com/g02/juegofx1/UtilidadPopUps.java
+++ src/main/java/com/g02/juegofx1/UtilidadPopUps.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 public class UtilidadPopUps {
@@ -20,15 +23,32 @@ public class UtilidadPopUps {
         // Crear una nueva ventana emergente
         Stage popupStage = new Stage();
 
+        // Remueve el marco de la ventana
+        popupStage.initStyle(StageStyle.UNDECORATED);
+
+        popupStage.initStyle(StageStyle.TRANSPARENT);
+
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+
+        // Crear un efecto de desenfoque (blur) para la ventana principal
+        BoxBlur blur = new BoxBlur(10, 10, 3);
+
+        Stage primaryStage = (Stage) App.scene.getWindow();
+
+        // Aplicar el efecto de desenfoque a la ventana principal
+        primaryStage.getScene().getRoot().setEffect(blur);
+
         // Mostrar el popup
         Scene popupScene = new Scene(popupRoot);
-
+        popupScene.setFill(Color.TRANSPARENT);
         popupStage.setScene(popupScene);
         popupStage.show();
 
         // Cerrar la ventana emergente después de un corto tiempo
         PauseTransition delay = new PauseTransition(Duration.seconds(1));
         delay.setOnFinished(event -> {
+            // Restaurar la escena y eliminar el efecto de desenfoque de la ventana principal
+            primaryStage.getScene().getRoot().setEffect(null);
             popupStage.close();
         });
         delay.play();
@@ -43,6 +63,21 @@ public class UtilidadPopUps {
         // Crear una nueva ventana emergente
         Stage popupStage = new Stage();
 
+        // Remueve el marco de la ventana
+        popupStage.initStyle(StageStyle.UNDECORATED);
+
+        popupStage.initStyle(StageStyle.TRANSPARENT);
+
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+
+         // Crear un efecto de desenfoque (blur) para la ventana principal
+        BoxBlur blur = new BoxBlur(10, 10, 3);
+
+        Stage primaryStage = (Stage) App.scene.getWindow();
+
+        // Aplicar el efecto de desenfoque a la ventana principal
+        primaryStage.getScene().getRoot().setEffect(blur);
+
         // Mostrar el popup
         Scene popupScene = new Scene(popupRoot);
 
@@ -52,6 +87,8 @@ public class UtilidadPopUps {
         // Cerrar la ventana emergente después de un corto tiempo
         PauseTransition delay = new PauseTransition(Duration.seconds(1));
         delay.setOnFinished(event -> {
+            // Restaurar la escena y eliminar el efecto de desenfoque de la ventana principal
+            primaryStage.getScene().getRoot().setEffect(null);
             popupStage.close();
         });
         delay.play();
