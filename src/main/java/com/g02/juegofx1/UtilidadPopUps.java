84eba8517bbaa4ac1c909d717621ffe3a07a11c4
KarenVT <karenkvot@gmail.com>
2023-06-22 15:10:10 -0500

    Método reutilizable en UtilidadPopUps para optimizar el código y evitar la duplicación

--- src/main/java/com/g02/juegofx1/UtilidadPopUps.java
+++ src/main/java/com/g02/juegofx1/UtilidadPopUps.java
@@ -15,9 +15,9 @@ import javafx.util.Duration;
 
 public class UtilidadPopUps {
 
-    public static void showCorrectPopup() throws IOException {
+    public static void showPopup(String fxmlPath) throws IOException {
         FXMLLoader loader = new FXMLLoader(
-                UtilidadPopUps.class.getResource("/com/g02/juegofx1/correctPopup.fxml"));
+                UtilidadPopUps.class.getResource(fxmlPath));
         Pane popupRoot = loader.load();
 
         // Crear una nueva ventana emergente
@@ -47,50 +47,19 @@ public class UtilidadPopUps {
         // Cerrar la ventana emergente después de un corto tiempo
         PauseTransition delay = new PauseTransition(Duration.seconds(1));
         delay.setOnFinished(event -> {
-            // Restaurar la escena y eliminar el efecto de desenfoque de la ventana principal
+            // Restaurar la escena y eliminar el efecto de desenfoque de la ventana
+            // principal
             primaryStage.getScene().getRoot().setEffect(null);
             popupStage.close();
         });
         delay.play();
+    }
 
+    public static void showCorrectPopup() throws IOException {
+        showPopup("/com/g02/juegofx1/correctPopup.fxml");
     }
 
     public static void showIncorrectPopup() throws IOException {
-        FXMLLoader loader = new FXMLLoader(
-                UtilidadPopUps.class.getResource("/com/g02/juegofx1/IncorrectPopup.fxml"));
-        Pane popupRoot = loader.load();
-
-        // Crear una nueva ventana emergente
-        Stage popupStage = new Stage();
-
-        // Remueve el marco de la ventana
-        popupStage.initStyle(StageStyle.UNDECORATED);
-
-        popupStage.initStyle(StageStyle.TRANSPARENT);
-
-        popupStage.initModality(Modality.APPLICATION_MODAL);
-
-         // Crear un efecto de desenfoque (blur) para la ventana principal
-        BoxBlur blur = new BoxBlur(10, 10, 3);
-
-        Stage primaryStage = (Stage) App.scene.getWindow();
-
-        // Aplicar el efecto de desenfoque a la ventana principal
-        primaryStage.getScene().getRoot().setEffect(blur);
-
-        // Mostrar el popup
-        Scene popupScene = new Scene(popupRoot);
-        popupScene.setFill(Color.TRANSPARENT);
-        popupStage.setScene(popupScene);
-        popupStage.show();
-
-        // Cerrar la ventana emergente después de un corto tiempo
-        PauseTransition delay = new PauseTransition(Duration.seconds(1));
-        delay.setOnFinished(event -> {
-            // Restaurar la escena y eliminar el efecto de desenfoque de la ventana principal
-            primaryStage.getScene().getRoot().setEffect(null);
-            popupStage.close();
-        });
-        delay.play();
+        showPopup("/com/g02/juegofx1/IncorrectPopup.fxml");
     }
 }
